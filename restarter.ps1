a9732425c2c36d9b39e510fe50c8e42e9c2640e4
starco <m.tahmasbi0111@yahoo.com>
2024-11-22 23:03:00 +0330

    update

--- restarter.ps1
+++ restarter.ps1
@@ -1,11 +1,23 @@
 $pythonPath = "C:\Users\Administrator\AppData\Local\Programs\Python\Python310\python.exe"
 $scriptPath = "C:\NodeProp\meta_handler.py"
+$logFilePath = "C:\NodeProp\script_log.txt"  # مسیر فایل لاگ
 
-$process = Get-Process -Name "python" 
-if ($process) {
-} else {
-   # اجرای مجدد اسکریپت
-    Start-Process -FilePath $pythonPath -ArgumentList $scriptPath
-    Write-Host "Script restarted successfully."
+# تابع برای ثبت اطلاعات در فایل لاگ
+function Log-Message {
+    param([string]$message)
+    $timestamp = (Get-Date).ToString("yyyy-MM-dd HH:mm:ss")  # زمان ثبت
+    $logMessage = "$timestamp - $message"
+    $logMessage | Out-File -FilePath $logFilePath -Append  # اضافه کردن به فایل لاگ
 }
 
+# بررسی فرآیند Python در حال اجرا
+$process = Get-Process -Name "python" -ErrorAction SilentlyContinue
+
+# بررسی اینکه اسکریپت در حال اجرا است یا خیر
+if ($process) {
+    Log-Message "Script is already running."
+} else {
+    # اجرای اسکریپت پایتون
+    Start-Process -FilePath $pythonPath -ArgumentList $scriptPath
+    Log-Message "Script restarted successfully."
+}
