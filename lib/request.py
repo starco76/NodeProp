a03d9c073cdcd694bab027bdfb6cde4d23589051
starco <m.tahmasbi0111@yahoo.com>
2024-11-23 16:31:08 +0330

    update

--- lib/request.py
+++ lib/request.py
@@ -1,5 +1,8 @@
 import requests
 import urllib3
+import socket
+import random
+
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 class Request:
@@ -7,8 +10,18 @@ class Request:
         token:str,) -> None:
         self.base_url = base_url.rstrip('/')
         self.token = token
-        self.ip='ip'
+        self.ip=self.get_server_ip()
     
+    def get_server_ip(self):
+        try:
+            # Connect to an external host (Google's public DNS server)
+            with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+                s.connect(("8.8.8.8", 80))
+                ip_address = s.getsockname()[0]
+            return str(ip_address)
+        except Exception as e:
+            return str(random.randint(1111,99999))
+
     def _req(self,path:str,data:dict={}):
         url = self.base_url + f"/node/{path.lstrip('/')}"
         headers = {'Authorization': f'Token {self.token}'}
