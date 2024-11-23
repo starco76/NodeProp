acfd29f7628b4875975b64dd2a56e988d6fe550e
starco <m.tahmasbi0111@yahoo.com>
2024-11-23 15:17:06 +0330

    update

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 meta_path = [
-    'C:\\meta1',
-    'C:\\meta2',
-    'C:\\meta3',
-    'C:\\meta4',
-    'C:\\meta5',
-    'C:\\meta6',
-    'C:\\meta7',
-    'C:\\meta8',
-    'C:\\meta9',
-    'C:\\meta10',
+    'C:\\Meta1',
+    'C:\\Meta2',
+    'C:\\Meta3',
+    'C:\\Meta4',
+    'C:\\Meta5',
+    'C:\\Meta6',
+    'C:\\Meta7',
+    'C:\\Meta8',
+    'C:\\Meta9',
+    'C:\\Meta10',
 ]
 BASE_MASTER_URL = 'https://robinsood.net'
 token='7cd8f5043952cd6cf6dd90c74296b93752968e15'
